f68c2721d964127f2ac5bc9f8191c528c19a0310
bit-modulator <neurapraxia@gmail.com>
2022-06-19 20:49:36 +0100

    Adding bar chart

--- client/src/components/Graphs/BarGraph.tsx
+++ client/src/components/Graphs/BarGraph.tsx
@@ -1,33 +1,28 @@
-import { useEffect, useState } from "react";
-
-export interface AirTraffic {
-  index?: number;
-  month: string;
-  airport: string;
-  total_flights: number;
-  cancelled_flights: number;
-  more_than_fifteen_minutes_early: number;
-  fifteen_to_one_minute_early: number;
-  zero_to_fifteen_minutes_late: number;
-  sixteen_to_thirty_minutes_late: number;
-  thirtyone_to_sixty_minutes_late: number;
-  sixtyone_to_onehundred_and_twenty_minutes_late: number;
-  onehundred_and_twentyone_to_onehundred_and_eighty_minutes_late: number;
-  onehundred_and_eightyone_to_threehundred_and_sixty_minutes_late: number;
-  more_than_threehundred_and_sixty_minutes_late: number;
-  average_delay_minutes: number;
-}
+import { SVGProps, useEffect, useRef, useState } from "react";
+import { AirTrafficData } from "../../types/airTrafficData";
+import * as d3 from "d3";
 
 const BarGraph = () => {
-  const [data, setData] = useState<AirTraffic[]>();
+  const [data, setData] = useState<AirTrafficData>();
+  const graph = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     fetch("http://localhost:5001/api/all")
       .then((response) => response.json())
-      .then((response) => console.log(response));
+      .then((response) => setData(response));
   }, []);
 
-  return <div>BarGraph</div>;
+  
+  useEffect(() => {
+    const svgElement = d3.select(graph.current)
+    svgElement.append("circle")
+      .attr("cx", 150)
+      .attr("cy", 70)
+      .attr("r",  50)
+  }, [])
+
+
+  return <svg ref={graph}></svg>;
 };
 
 export default BarGraph;
---
+++ client/src/types/airTrafficData.ts
@@ -0,0 +1,21 @@
+export interface AirTrafficData {
+  data: AirTraffic[];
+}
+
+export interface AirTraffic {
+  index?: number;
+  month: string;
+  airport: string;
+  total_flights: number;
+  cancelled_flights: number;
+  more_than_fifteen_minutes_early: number;
+  fifteen_to_one_minute_early: number;
+  zero_to_fifteen_minutes_late: number;
+  sixteen_to_thirty_minutes_late: number;
+  thirtyone_to_sixty_minutes_late: number;
+  sixtyone_to_onehundred_and_twenty_minutes_late: number;
+  onehundred_and_twentyone_to_onehundred_and_eighty_minutes_late: number;
+  onehundred_and_eightyone_to_threehundred_and_sixty_minutes_late: number;
+  more_than_threehundred_and_sixty_minutes_late: number;
+  average_delay_minutes: number;
+}
